3e838e149305f349b6f1074db2494b2b0de37e8a
SebastianPagacz <234595@student.uek.krakow.pl>
2026-04-14 22:41:34 +0200

    localforage, updated manifest and service-worker

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,34 @@
 const cacheName = "dtmf_cache";
 const cachedItems = [
-    "index.html",
+    "/",
+    "manifest.json",
+    "/index.html",
     "src/css/style.css",
     "src/js/main.js",
+    "src/js/localforage.js",
     "src/img/icons/camera-icon.avif",
-    "src/img/icons/folder-icon.avif"
+    "src/img/icons/folder-icon.avif",
+    "icon/dtmf_icon_144x144.png",
+    "icon/dtmf_icon.png",
+    "icon/logo-500x500.avif"
 ];
 
-self.addEventListener("install", (e) =>{
-    console.log("installed!");
+self.addEventListener("install", (e) => {
+    console.log("installed!"); // debug
     
     e.waitUntil(
         caches.open(cacheName).then((cache) =>{
-            console.log("cached");
+            console.log("cached"); // debug
             return cache.addAll(cachedItems);
         })
     );
 });
+
+self.addEventListener("fetch", (e) => {
+    e.respondWith(
+        caches.match(e.request, {ignoreSearch: true}).then(response => {
+            console.log(response);
+            return response || fetch(e.request);
+        })
+    )
+})
--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,34 @@
-let swRegistration = null;
+import { createFolder } from "./folders.js";
 
-if ("serviceWorker" in navigator){
-    navigator.serviceWorker
-        .register("service-worker.js")
-        .then((reg) => {
-            swRegistration = reg;
-        });
+const init = async () => {
+    let swRegistration = null;
+
+    if ("serviceWorker" in navigator){
+        navigator.serviceWorker
+            .register("service-worker.js")
+            .then((reg) => {
+                swRegistration = reg;
+            });
+    }
+
+    async function initDb(){
+        let folders = await localforage.getItem('folders');
+        if (!folders){
+            folders = {};
+            await localforage.setItem('folders', folders);
+            return folders;
+        }
+    }
+
+    initDb();
+
+    const folderBtn = document.getElementById('folderBtn');
+
+    folderBtn.addEventListener('click', () => {
+        const folderName = document.getElementById('folderName').value;
+        createFolder(folderName);
+    });
+
 }
+
+document.addEventListener("DOMContentLoaded", init);
